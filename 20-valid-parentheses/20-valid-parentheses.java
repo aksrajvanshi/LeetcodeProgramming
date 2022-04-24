922a0b2afeee365dc1d31be65d2c5b40f2034a2c
aksrajvanshi <aksrajvanshi@gmail.com>
2022-04-24 11:57:09 -0700

    Time: 6 ms (5.91%), Space: 42.6 MB (12.45%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -6,40 +6,27 @@ class Solution {
         }
         
         Stack<Character> st = new Stack<>();
+        Map<Character, Character> hmap = new HashMap<>();
+        
+        hmap.put(')','(');
+        hmap.put(']','[');
+        hmap.put('}','{');
+        
         for(int i=0; i < s.length(); i++){
             if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '['){
                 st.push(s.charAt(i));
             }
-            
-            if(st.isEmpty()){
-                if(s.charAt(i) == ')' || s.charAt(i) == '}' || s.charAt(i) == ']'){
+            else{
+                if(st.isEmpty()){
                     return false;
                 }
-            }
-            else{
-                if(s.charAt(i) == ')'){
-                    if(st.peek() == '('){
-                        st.pop();
-                    } else{
-                        return false;
-                    }
+                else if(hmap.get(s.charAt(i)).equals(st.peek())){
+                    st.pop();
+                }
+                else{
+                    return false;
                 }
                 
-                if(s.charAt(i) == '}'){
-                    if(st.peek() == '{'){
-                        st.pop();
-                    } else {
-                        return false;
-                    }
-                }
-                
-                if(s.charAt(i) == ']'){
-                    if(st.peek() == '['){
-                        st.pop();
-                    } else{
-                        return false;
-                    }
-                }
             }
         }
         
