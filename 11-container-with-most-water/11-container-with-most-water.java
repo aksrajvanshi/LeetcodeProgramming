1dcb3b50aa7c21f0edf334b2e0b512ea7e73104f
aksrajvanshi <aksrajvanshi@gmail.com>
2022-04-30 16:51:25 -0700

    Time: 5 ms (42.79%), Space: 81 MB (41.72%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.java
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -7,7 +7,15 @@ class Solution {
         int left = 0;
         int right = height.length-1;
         int res = Integer.MIN_VALUE;
+        /*
         
+        it's a 2-pointer approach, where the lesser height endpoint is responsible for less water.
+        Hence, we have left and right pointers at 0th and (height.length-1)th index. 
+        
+        if height of left index is smaller than height of larger index, we increment the left index, and vice-versa. 
+        Calculate the area in every case and find the max of it.
+        
+        */
         while(left < right){
             
             int area = Math.min(height[left], height[right]) * (right-left);
