51fb3eecb7d7f6593e763c0e16f3cbaabeeeeb38
aksrajvanshi <aksrajvanshi@gmail.com>
2022-06-17 23:08:32 -0700

    Time: 122 ms (5.06%), Space: 94.7 MB (5.01%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.java
@@ -0,0 +1,49 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        
+        int[] indegree = new int[numCourses];
+        int[][] grid = new int[numCourses][numCourses];
+        for(int[] edge: prerequisites){
+            int source = edge[0];
+            int dest = edge[1];
+            indegree[dest]++;
+            
+            grid[source][dest] = 1;
+        }
+        
+        Queue<Integer> q = new LinkedList<>();
+        for(int node=0; node < indegree.length; node++){
+            if(indegree[node] == 0){
+                q.add(node);
+            }
+        }
+        
+        if(q.isEmpty()){
+            return false;
+        }
+        
+        List<Integer> topoSort = new ArrayList<>();
+        System.out.println("Queue before loop:: " + q);
+        
+        while(!q.isEmpty()){
+            
+            int current = q.remove();
+            topoSort.add(current);
+            
+            for(int rowIdx=0; rowIdx < grid[current].length; rowIdx++){
+                if(current!= rowIdx && grid[current][rowIdx] == 1){
+                    indegree[rowIdx]--;
+                    
+                    if(indegree[rowIdx] == 0){
+                        q.add(rowIdx);
+                    }
+                }
+            }
+            
+            System.out.println("Queue inside loop after iteration:: " + q);
+            
+        }
+        
+        return topoSort.size() == numCourses;
+    }
+}
